278c808b41f8e192ce4e7d6c1befb1f04ad95214
najma-ahmed <najmahmed20162016@gmail.com>
2022-11-21 20:22:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+var cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat(){
+    var destructivelyRemoveLastCat = cats.shift()
+    return destructivelyRemoveLastCat
+}
+
+function destructivelyRemoveFirstCat(){
+    var destructivelyRemoveFirstCat = cats.shift()
+    return destructivelyRemoveFirstCat
+}
+function appendCat(name){
+    const catArr = [...cats, name]
+    return catArr
+}
+function removeFirstCat(){
+    const firstItem = cats.slice(1)
+    return firstItem
+}
+
+
